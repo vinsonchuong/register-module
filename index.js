3a8217ddddc10840caece2795ebf7185ee3da5ff
Vinson Chuong <vinsonchuong@gmail.com>
2016-07-06 13:08:43 -0700

    Log arguments

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const Module = require('module').Module;
 module.exports = function(modules) {
   for (const name of Object.keys(modules)) {
     const {path, main} = modules[name];
+    console.log(name, path, main);
   }
 };
